4a716a888e53e0678510f59f4ff206499f470e42
Chris Lewis <chrissam42@gmail.com>
2016-03-16 20:52:44 -0700

    experimental column wrapping JS for when section content is taller than the window

--- js/bungee-website.js
+++ js/bungee-website.js
@@ -1,21 +1,47 @@
 $(function() {
     "use strict";
-    
+
     var win = $(window), page = $('body');
+
+    // limit jumping around when resizing window
     var scrollRatio=0;
-    var setScrollRatio = function() {
+    function setScrollRatio() {
         var scrollTop = win.scrollTop();
         var scrollLeft = win.scrollLeft();
         scrollRatio = scrollLeft ? scrollLeft / page.width() : scrollTop / page.height();
-        console.log(scrollRatio);
-    };
+    }
+    
     win.on('scroll', setScrollRatio);
+
+    // do horizontal column wrapping if content overflows
+    function resetColumns() {
+        $('section').css({'column-count': '', 'width': ''});
+    }
+
+    function calculateColumns() {
+        $('section').each(function() {
+            var section = $(this);
+            var contentHeight = section.height();
+            var windowHeight = page.height();
+            var columns = Math.ceil(contentHeight / windowHeight);
+            if (columns > 1) {
+            console.log(this.id, columns, section.width());
+                var gap = parseFloat(section.css('column-gap'));
+                section.width(section.width()*columns + gap*(columns-1));
+                section.css('column-count', columns);
+            }
+        });
+    }
+
+    calculateColumns();
+
     var resizing = false;
     win.on('resize', function() {
         if (resizing) {
             clearTimeout(resizing);
         } else {
             win.off('scroll', setScrollRatio);
+            resetColumns();
         }
         var scrollTop = win.scrollTop();
         var scrollLeft = win.scrollLeft();
@@ -26,6 +52,7 @@ $(function() {
         }
         resizing = setTimeout(function() {
             win.on('scroll', setScrollRatio);
+            calculateColumns();
         }, 1000);
     });
 });
