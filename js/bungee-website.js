f8c42a653313a60db5f967dafc39b99ba40ec3db
Chris Lewis <chrissam42@gmail.com>
2016-06-11 08:15:00 -0700

    Various fixes for bookmarking sections in horizontal mode

--- js/bungee-website.js
+++ js/bungee-website.js
@@ -3,30 +3,27 @@ $(function() {
 
     var win = $(window), page = $('body'), article = $('article');
 
+    //in horizontal mode, the scrolling element is <article> not the window or <html>
+    function elementOffset(el) {
+        return $(el).offset().left + article.scrollLeft();
+    }
+
     // horizontal scroll to hash
     function goToHash() {
         var pos, bookmark = $(window.location.hash);
         if (bookmark.length) {
-            pos = bookmark.offset().left + article.scrollLeft();
-            if (pos > win.width()/2) {
+            pos = elementOffset(bookmark);
+            if (Math.abs(article.scrollLeft()-pos) > win.width()/2) {
                 article.animate({'scrollLeft':pos}, {'duration':'0.5'});
             }
         }
         return false;
     }
 
-    window.location.hash && goToHash();
-    win.on('hashchange', goToHash);
-
-    // limit jumping around when resizing window
-    var scrollRatio=0;
-    function setScrollRatio() {
-        var scrollTop = win.scrollTop();
-        var scrollLeft = win.scrollLeft();
-        scrollRatio = scrollLeft ? scrollLeft / page.width() : scrollTop / page.height();
+    if (window.location.hash) {
+        $(window).on('load', goToHash);
     }
-    
-    win.on('scroll', setScrollRatio);
+    win.on('hashchange', goToHash);
 
     // do horizontal column wrapping if content overflows
 /*
@@ -52,24 +49,63 @@ $(function() {
     calculateColumns();
 */
 
+    // limit jumping around when resizing window
+    var scrollRatio=0;
+    var sectionOffsets = [];
+    function doScrollStuff() {
+        var scrollTop = win.scrollTop();
+        var scrollLeft = article.scrollLeft();
+        var pageWidth = article.prop('scrollWidth');
+        var pageHeight = article.prop('scrollHeight');
+        scrollRatio = pageWidth > pageHeight ? scrollLeft / pageWidth : scrollTop / pageHeight;
+    
+        //set URL hash for current section
+        for (var i in sectionOffsets) {
+            if (sectionOffsets[i][1] > scrollLeft) {
+                if (i > 0) {
+                    history.replaceState({}, "", sectionOffsets[i-1][0]);
+                }
+                break;
+            }
+        }
+    }
+
+    function watchScroll() {
+        $('article').add(window).on('scroll', doScrollStuff);
+    }
+    
+    function unwatchScroll() {
+        $('article').add(window).off('scroll', doScrollStuff);
+    }
+    
+    watchScroll();
+
     var resizing = false;
+    function doResizeStuff() {
+        sectionOffsets = [];
+        $('section').each(function() {
+            sectionOffsets.push(['#' + this.id, elementOffset(this)]);
+        });
+        watchScroll();
+//        calculateColumns();
+    }
+    
+    $(window).on('load', doResizeStuff);
+    
     win.on('resize', function() {
         if (resizing) {
             clearTimeout(resizing);
         } else {
-            win.off('scroll', setScrollRatio);
+            unwatchScroll();
 //            resetColumns();
         }
         var scrollTop = win.scrollTop();
-        var scrollLeft = win.scrollLeft();
+        var scrollLeft = article.scrollLeft();
         if (scrollLeft) {
-            win.scrollLeft(page.width() * scrollRatio);
+            article.scrollLeft(article.prop('scrollWidth') * scrollRatio);
         } else {
             win.scrollTop(page.height() * scrollRatio);
         }
-        resizing = setTimeout(function() {
-            win.on('scroll', setScrollRatio);
-//            calculateColumns();
-        }, 1000);
+        resizing = setTimeout(doResizeStuff, 1000);
     });
 });
