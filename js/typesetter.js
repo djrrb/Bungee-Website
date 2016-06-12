7e7a43659c61c7ed2100e2a887d8dc06cf932f31
Chris Lewis <chrissam42@gmail.com>
2016-06-11 22:33:14 -0700

    working permalink

--- js/typesetter.js
+++ js/typesetter.js
@@ -44,35 +44,11 @@
         var orientationcontrols = $('#controls input[name=orientation]');
         //var rotatedcontrol = $('#controls input[name=rotated]');
         var altcontrols = $('#controls input[name=alt]');
-        //var sizecontrol = $('#controls input[name=size]');
+        var sizecontrol = $('#controls input[name=size]');
         //var autofit = $('#controls input[name=autofit]');
         var textcontrol = $('#controls input[name=text]');
         var backgroundcontrols = $('#background-controls input');
         
-        //process initial url
-        if (window.location.hash.length > 1) {
-            //$('input[type=checkbox], input[type=radio]').prop('checked', false);
-            $.each(window.location.hash.substr(1).split('&'), function(i,clause) {
-                var eq = clause.split('=', 2);
-                switch (eq[0]) {
-                    case 'text':
-                        textcontrol.val(Bungee.cleanupText(decodeURIComponent(eq[1].replace(/\+/g, '%20'))));
-                        break;
-/*
-                    case 'size':
-                        sizecontrol.val(eq[1]);
-                        break;
-                    case 'fit':
-                        autofit.prop('checked', !eq[1].match(/false|0|no/i));
-                        break;
-*/
-                    default:
-                        allcontrols.filter('[name=' + eq[0] + '][value="' + eq[1] + '"]').prop('checked', true);
-                        break;
-                }
-            });
-        }
-
         function doCode() {
             var tab = "  ";
             var code = "";
@@ -128,20 +104,24 @@
             $('#code').html(code);
         }
     
+        function colorToLayerClass(color) {
+            var layerclass = color.toHex();
+            if (color.getAlpha() < 1) {
+                layerclass += '-' + (color.getAlpha()*100);
+            }
+            return layerclass;
+        }
+    
         function setURL() {
             var url = $('#controls').serialize(); // + '&text=' + encodeURIComponent(preview.find('span').first().text().trim());
-            if (window.history && history.pushState) {
-                history.pushState({}, '', '#' + url);
-            } else {
-                window.location.hash = '#' + url;
-            }
+            $('#permalink').prop('href', '?' + url + '#' + preview.closest('section').prop('id'));
         }
         
         function doSVG() {
             var reference = preview.find('.layer.text');
             var req = {};
             req.text = Bungee.cleanupText(textcontrol.val()); //reference.find('span').first().text().trim();
-            req.size = 144; //sizecontrol.val();
+            req.size = sizecontrol.val();
             req.orientation = orientationcontrols.filter(':checked').val();
             req.layers = {};
             layercontrols.filter(':checked').each(function() {
@@ -187,17 +167,18 @@
             req.format='pdf';
             $('#pdf').attr('href', '/svg.php?' + $.param(req));
         }
-    
+
         function updatePreview(evt) {
             var actor, temp;
-            if (evt) {
+
+            if (evt && evt !== jQuery) {
                 //evt will either be a real event, or an element
                 actor = evt.target || evt;
             } else {
                 actor = textcontrol.get(0);
             }
 
-             if (actor.tagName === 'LABEL') {
+            if (actor.tagName === 'LABEL') {
                 // this will be called again for the actual input element
                 return;
             }
@@ -214,14 +195,6 @@
             var preset = Bungee.presets[presetcontrols.filter(':checked').val()] || {};
 
             var layers = [];
-            function colorToLayerClass(layer, color) {
-                var layerclass = layer;
-                layerclass += '-' + color.toHex();
-                if (color.getAlpha() < 1) {
-                    layerclass += '-' + (color.getAlpha()*100);
-                }
-                return layerclass;
-            }
             if (presetcontrols.is(actor)) {
                 //reset layer checkboxes on preset change
                 $.each(preset, function(layer, color) {
@@ -236,15 +209,16 @@
                     }
                     layercontrol.spectrum('set', newcolor);
                     handleColor(layercontrol, on && newcolor, true);
+                    var layerclass = colorToLayerClass(newcolor);
                     if (on) {
-                        layers.push(colorToLayerClass(layer, newcolor));
+                        layers.push(layer + '-' + layerclass);
                     }
                 });
             } else {
                 //otherwise read layers from checkboxes
                 layercontrols.filter(':not(.none)').each(function() {
                     var layer = $(this);
-                    layers.push(colorToLayerClass(layer.data('layer'), layer.spectrum('get')));
+                    layers.push(layer.data('layer') + '-' + colorToLayerClass(layer.spectrum('get')));
                 });
                 if (layercontrols.filter('.background.none').length) {
                     layers.push('background-transparent');
@@ -283,11 +257,11 @@
                     }
                 }
             }
-            
+
             var begin=backgroundcontrols.filter('[name=begin]:checked').val(),
                 end=backgroundcontrols.filter('[name=end]:checked').val(),
                 block=backgroundcontrols.filter('[name=block]:checked').val();
-            
+
             if (begin && end) {
                 classes.push('begin-' + begin);
                 classes.push('end-' + end);
@@ -299,7 +273,7 @@
             altcontrols.filter(':checked').each(function() {
                 classes.push('alt-' + this.value);
             });
-            
+
             //update the preview!
             preview.prop('className', classes.join(' ')).text(text);
             Bungee.init(preview);
@@ -320,8 +294,9 @@
                 }
             }
 
+            setURL();
+
             if (false && evt) {
-                setURL();
                 setTimeout(doCode);
                 if (evt.type !== 'input') {
                     // don't update SVG while slider is moving
@@ -333,11 +308,13 @@
         function handleColor(layer, newcolor, silent) {
             var cssrule = 'background-color'; //layer.hasClass('background') ? 'background-color' : 'color';
             if (newcolor) {
+                layer.next().val(newcolor.toHex8());
                 layer.removeClass('none').css(cssrule, newcolor.toRgbString());
                 if (layer.hasClass('background')) {
                     $('#tester').css('background-color', newcolor.toRgbString());
                 }
             } else {
+                layer.next().val("");
                 layer.addClass('none').css(cssrule, 'transparent');
                 if (layer.hasClass('background')) {
                     $('#tester').css('background-color', '');
@@ -362,16 +339,57 @@
                 'preferredFormat': 'hex',
                 'appendTo': '#tester',
                 'move': function(newcolor) {
+                    presetcontrols.prop('checked', false);
                     handleColor(layer, newcolor);
                     if (!newcolor) {
                         layer.spectrum('hide');
                     }
                 }
             });
+            //create hidden input for ease of serialization
+            layer.after('<input type="hidden" class="layercolors" name="' + layer.data('layer') + '" value="' + layer.spectrum('get').toHex8() + '">');
         });
 
-        allcontrols.on('change', updatePreview);
-        textcontrol.on('keyup', updatePreview);
+        //process initial url
+        if (window.location.search.length > 1) {
+            //$('input[type=checkbox], input[type=radio]').prop('checked', false);
+            $.each(window.location.search.substr(1).split('&'), function(i,clause) {
+                var eq = clause.split('=', 2);
+                var input, layer;
+                switch (eq[0]) {
+                    case 'text':
+                        textcontrol.val(Bungee.cleanupText(decodeURIComponent(eq[1].replace(/\+/g, '%20'))));
+                        break;
+/*
+                    case 'size':
+                        sizecontrol.val(eq[1]);
+                        break;
+                    case 'fit':
+                        autofit.prop('checked', !eq[1].match(/false|0|no/i));
+                        break;
+*/
+                    case 'inline': case 'outline': case 'regular': case 'shade': case 'background': case 'sign':
+                        input = $('#controls input.layercontrols[name=' + eq[0] + ']').val(eq[1]);
+                        layer = layercontrols.filter('.' + eq[0]);
+                        if (eq[1]) {
+                            layer.spectrum('set', eq[1]);
+                            handleColor(layer, layer.spectrum('get'), true);
+                        } else {
+                            handleColor(layer, null, true);
+                        }
+                        break;
+
+                    default:
+                        input = allcontrols.filter('[name=' + eq[0] + '][value="' + eq[1] + '"]');
+                        input.prop('checked', true);
+                        break;
+                }
+            });
+        }
+
+        updatePreview();
+        //doCode();
+        //doSVG();
 
         // while sliding size slider, don't do a full update but just modify size
         /*
@@ -380,8 +398,8 @@
         });
         */
 
-        updatePreview();
-        //doCode();
-        //doSVG();
+        allcontrols.on('change', updatePreview);
+        textcontrol.on('keyup', updatePreview);
+
     }); //window.onload
 })();
